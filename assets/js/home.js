27f84b1c1e9b2882e8558946e0f2d068ec705a00
Atticus White <contact@atticuswhite.com>
2015-08-13 03:11:06 -0400

    carousel typa shit

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,11 +1,30 @@
 (function ($) {
+  function zoomCarousel (slides) {
+    var position = -1;
+
+    function transition () {
+      $(slides).removeClass('active');
+      if (++position >= slides.length) {
+        position = 0;
+      }
+      $(slides[position]).addClass('active');
+    }
+
+    transition();
+
+    setInterval(transition, 10000);
+  }
+
+
   $(document).ready(function () {
     $('#typed-headline span').typed({
-      strings: ['States', 'Nurses', 'Veterans', 'You'],
+      strings: ['iowa', 'nurses', 'veterans', 'you'],
       backDelay: 2000,
       backSpeed: 20,
       typeSpeed: 20,
       showCursor: false
     });
+
+    zoomCarousel($('#content .slides .slide'));
   });
 })(jQuery);
--- index.php
+++ index.php
@@ -1,16 +1,32 @@
-<?php get_header(); ?>
+<?php
+get_header();
+$slides = array(
+  'https://berniesanders.com/wp-content/uploads/2015/08/080815_Bernie_SeaTac-6456.jpg',
+  'https://berniesanders.com/wp-content/uploads/2015/08/0810151927-1.jpg'
+)
+?>
+
+
 
 <main class="bs-docs-masthead" id="content" role="main">
   <div class="container">
-    <h2 id="typed-headline"><span>States</span> for Bernie Sanders</h2>
-    <a href="#" class="btn btn-primary btn-lg">Get Yours Today</a>
+    <div class="slides">
+      <?php foreach ($slides as $slide): ?>
+        <div class="slide" style="background-image: url(<?php echo $slide; ?>)"></div>
+      <?php endforeach; ?>
+    </div>
+    <div class="content">
+      <h1>Sites for Bernie Sanders</h1>
+      <h2 id="typed-headline"><span>states</span>.forberniesanders.com</h2>
+      <a href="#" class="btn btn-primary btn-lg">Claim Yours Today</a>
+    </div>
   </div>
 </main>
 <div class="bs-docs-featurette">
   <div class="container">
     <h2 class="bs-docs-featurette-title">Made for everyone, everywhere.</h2>
     <p class="lead">
-      Sites ForBernieSanders makes it easy for you to get your own grassroots website. <a href="#">CodersForSandres</a> has created a network of sites ready-to-go for your Bernie Sanders group.
+      Sites ForBernieSanders makes it easy for you to get your own grassroots website. <a href="#">CodersForSandres</a> has created a network of sites ready-to-go for your Bernie Sanders group. Just like Bernie, we don't want your money. Only your support.
     </p>
   </div>
 </div>
