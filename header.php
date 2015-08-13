361f3362c9f188e68688406da9560d5bb5cb071a
Atticus White <contact@atticuswhite.com>
2015-08-13 02:01:18 -0400

    header and footer

---
+++ header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html class="no-js" <?php language_attributes(); ?>>
+  <head>
+  	<title><?php wp_title('|', true, 'right'); ?></title>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='http://fonts.googleapis.com/css?family=Muli:300,400,300italic,400italic|Merriweather:900,700,700italic' rel='stylesheet' type='text/css'>
+    <link rel="shortcut icon" href="https://berniesanders.com/wp-content/themes/berniesanders2016/favicon.ico">
+    <meta name="theme-color" content="#147FD7"/>
+    <?php wp_head(); ?>
+  </head>
+  <body <?php body_class(); ?>>
