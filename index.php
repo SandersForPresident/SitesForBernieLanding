5bed75513f8112749f5b43aaaf64462c3c1e045e
Atticus White <contact@atticuswhite.com>
2015-08-13 06:19:39 -0400

    grassroots

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ $slides = array(
   <div class="container">
     <h2 class="bs-docs-featurette-title">Made for everyone, everywhere.</h2>
     <p class="lead">
-      Sites ForBernieSanders makes it easy for you to get your own grassroots website. <a href="#">CodersForSandres</a> has created a network of sites ready-to-go for your Bernie Sanders group. Just like Bernie, we don't want your money. Only your support.
+      Sites ForBernieSanders makes it easy for you to get your own grassroots website. <a href="http://reddit.com/r/SandersForPresident">GrassrootsForSanders</a> has created a network of sites ready-to-go for your Bernie Sanders group. Just like Bernie, we don't want your money. Only your support.
     </p>
   </div>
 </div>
