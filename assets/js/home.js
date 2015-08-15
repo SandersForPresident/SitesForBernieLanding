e01c6fb68ca65c559f41c4650a8a64fe98e2a71a
Atticus White <contact@atticuswhite.com>
2015-08-15 01:57:29 -0400

    banner.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,25 +1,4 @@
 (function ($) {
-  function zoomCarousel (slides) {
-    var position = -1;
-
-    function transition () {
-      $(slides).removeClass('active');
-      if (++position >= slides.length) {
-        position = 0;
-      }
-      $(slides[position]).addClass('active');
-    }
-
-    transition();
-
-    setInterval(transition, 10000);
-  }
-
-  function updateBannerHeight () {
-    var height = $(window).height() - $('header').height();
-    $('#banner').css('height', height + 'px');
-  }
-
   $(document).ready(function () {
     $('#typed-headline span').typed({
       strings: ['iowa', 'nurses', 'veterans', 'you'],
@@ -28,11 +7,6 @@
       typeSpeed: 20,
       showCursor: false
     });
-
-    updateBannerHeight();
-    $(window).on('resize', updateBannerHeight);
-
-    zoomCarousel($('#banner .slides .slide'));
     $('#banner .btn, #get-site').click(function () {
       $('html,body').animate({
         scrollTop: $('#claim').offset().top
