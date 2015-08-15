458e593f90ed9492e5696476794fa357bb863678
Atticus White <contact@atticuswhite.com>
2015-08-15 01:54:48 -0400

    map.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -20,45 +20,6 @@
     $('#banner').css('height', height + 'px');
   }
 
-  function getSiteByState(code) {
-    for (var i = 0; i < window.sites.length; i++) {
-      if (code.toUpperCase() === window.sites[i].state) {
-        return window.sites[i];
-      }
-    }
-    return null;
-  }
-
-  function buildMap () {
-    var colors = window.sites.reduce(function (map, site) {
-      if (site.state) {
-        map[site.state.toLowerCase()] = '#147fd7';
-      }
-      return map;
-    }, {});
-
-    $('#map').vectorMap({
-      map: 'usa_en',
-      colors: colors,
-      enableZoom: false,
-      backgroundColor: null,
-      onRegionClick: function (element, code, region) {
-        var site = getSiteByState(code);
-        if (site) {
-          window.location = site.url;
-        } else {
-          $('html, body').animate({
-            scrollTop: $('#claim').offset().top
-          });
-          $('#claim-form input[name=organization]').val('State of ' + region);
-          $('#claim-form input[name=cause]').val('Local Communities');
-          $('#claim-form input[name=url]').val(code);
-        }
-      }
-    });
-  }
-
-
   $(document).ready(function () {
     $('#typed-headline span').typed({
       strings: ['iowa', 'nurses', 'veterans', 'you'],
@@ -72,8 +33,6 @@
     $(window).on('resize', updateBannerHeight);
 
     zoomCarousel($('#banner .slides .slide'));
-    buildMap();
-
     $('#banner .btn, #get-site').click(function () {
       $('html,body').animate({
         scrollTop: $('#claim').offset().top
