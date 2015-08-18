6e8b8df7053017aa2ba048c89120138c16bba8a6
Atticus White <contact@atticuswhite.com>
2015-08-18 13:18:57 -0400

    compression to assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    minifyCss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify');
 
 gulp.task('less', function () {
   gulp.src('assets/less/main.less')
   .pipe(less())
+  .pipe(minifyCss())
   .pipe(gulp.dest('dist/'));
 });
 
@@ -16,12 +19,14 @@ gulp.task('js:vendor', function () {
     'bower_components/jquery-throttle-debounce/jquery.ba-throttle-debounce.js'
   ])
   .pipe(concat('vendor.js'))
+  .pipe(uglify())
   .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('js:client', function () {
   gulp.src('assets/js/**/*.js')
   .pipe(concat('site.js'))
+  .pipe(uglify())
   .pipe(gulp.dest('dist/'));
 });
 
